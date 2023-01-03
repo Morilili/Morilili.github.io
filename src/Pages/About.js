19b868c4d7c7ecc9bcad64338db9925cdb47866c
Morilili <Morilili@users.noreply.github.com>
2023-01-03 00:37:58 -0800

    Update About Page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,25 +1,40 @@
 import { Tooltip } from '@mui/material';
 import React from 'react'
 import './About.css';
+import Typed from 'react-typed';
 
 function About() {
   return (
     <div className='top-container'>
       <br />
       <div id='about'>
-        <div id='description' style={{display:'flex'}}>
-          <h1 id='name-about' >
+        <div id='description'>
+          <h1 id='name-about'>
             Morris Ho
-
           </h1>
+          <p className='typed-box'>
+            <b className='typed-box' id='typed'>
+              <Typed 
+                strings={[
+                  "2nd year U of T Student",
+                  "Software Developer",
+                  "Football Player",
+                  "Classical Music Ethusiast",
+                  "Entrepreneur (Aspiring)"
+                ]}
+                typeSpeed={50}
+                backSpeed={80}
+                loop
+              />
+            </b>
+          </p>
           
-          <div id='pp_container'>
-            <img id='pic' alt='1' src={require("../assets/morris_pro.jpg")}></img>
-            <Tooltip title='Moments before disaster (☉_☉)'>
-              <img id='pic2' class="hover_img" alt='2' src={require("../assets/IMG-8255.JPG")}></img>
-            </Tooltip>
+          <img className='pic orig' alt='1' src={require("../assets/morris_pro.jpg")}></img>
+          <Tooltip title='Moment before disaster (☉_☉)'>
+            <img className="pic hover_img" alt='2' src={require("../assets/IMG-8255.JPG")}></img>
+          </Tooltip>
             
-          </div>
+          
         </div>
         
       </div>
